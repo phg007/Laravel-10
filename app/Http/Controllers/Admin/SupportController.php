07814d5774f18e1d3aee7ba5f93be5a89c44d857
paulo.goncalves <paulo.henriqueicm2011@gmail.com>
2023-05-17 22:54:50 -0300

    charme:Migrate

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Support;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin/supports/index');
+
+        $supports = $support->all();
+        
+        return view('admin/supports/index', compact('supports'));
     }
 }
